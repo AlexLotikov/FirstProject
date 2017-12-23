fbac7144bc077fab1fb4a684baa00c1bb920186f
Alex <alexlotikov@mail.ru>
2017-12-23 12:21:38 +0300

    Добавление цикла
    Signed-off-by: Alex <alexlotikov@mail.ru>

---
+++ src/main/java/com/alex/app/TestProject/App.java
@@ -0,0 +1,17 @@
+package com.alex.app.TestProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        int i = 0;
+        for (i;i<5;i++) {
+        	System.out.print(i	);
+        }
+    }
+}
