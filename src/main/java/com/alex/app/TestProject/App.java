c1934df4ed8f522cc7aee4acc20792ffc2c5b8f2
Alex <alexlotikov@mail.ru>
2017-12-24 12:46:32 +0300

    Fix bag

--- src/main/java/com/alex/app/TestProject/App.java
+++ src/main/java/com/alex/app/TestProject/App.java
@@ -1,20 +1,11 @@
 package com.alex.app.TestProject;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-<<<<<<< HEAD
-        int i = 0;
-        for (i;i<5;i++) {
-        	System.out.print(i	);
-        }
-=======
->>>>>>> 9d45330acdf66e4f438b66739d9e9d296ff61935
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+		for (int i = 0; i < 5; i++) {
+			System.out.print(i);
+		}
+	}
 }
